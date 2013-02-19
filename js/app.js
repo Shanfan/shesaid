f223b3d26b26141261ecbcd4383f879a3b3e6ad6
Shanfan <chitchy@gmail.com>
2013-02-19 16:41:53 -0500

    prepare for merge master

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
     
     //this will the init function later
     var quoteID = 0;
-    
+    //var autoPlay = window.setInterval(function(){showNext();}, 1500);
     
     $(".controller .icon-shuffle").click(function(e){
         e.preventDefault();
@@ -20,23 +20,23 @@
         
         if ($(this).hasClass("icon-pause")){
             $(this).removeClass("icon-pause").addClass("icon-play");
-            appControl.pause();
+            window.clearInterval(autoPlay);
         } else {
-            $(this).removeClass("icon-play").addClass("icon-pause");
-            appControl.autoplay();
+            $(this).removeClass("icon-play").addClass("icon-pause");            
+            autoPlay =  window.setInterval(function(){
+                showNext();
+            }, 1500);
         };
     });
     
     $(".controller .icon-next").click(function(e){
-        e.preventDefault(); 
-        if (quoteID < quotes.length-1) { quoteID += 1; } else { quoteID = 0; }
-        showQuote(quoteID);
+        e.preventDefault();
+        showNext();
     });
     
     $(".controller .icon-prev").click(function(e){
         e.preventDefault();
-        if (quoteID == 0 ) {quoteID = 17; } else { quoteID -= 1; }
-        showQuote(quoteID);
+        showPrev();
     });
     
     
@@ -59,9 +59,9 @@
 
 
 
-// appControl plugin
+// appControl object
 
-var appControl = {
+/*var appControl = {
     shuffle : true,
     
     init : function(){
@@ -76,12 +76,9 @@ var appControl = {
     prev : function(){
         //show previous quote
     },
-    
-    next : function(n){
-        //show next quote
+*/
 
-    },
-
+/*
     pause : function(){
         this.autoplay = null;
     },
@@ -94,18 +91,26 @@ var appControl = {
         
     }
 
-};
+};*/
 
 function showQuote(n) {
         var quote = quotes[n].line;
         var chara = quotes[n].by;
         var season = "Season" + quotes[n].inSeason;
         
-        console.log("n = " + n +", chara = " + chara + ", season = " + season)
-        
         $(".app .view blockquote").text(quote);
         $(".app .metadata .by").text(chara);
         $(".app .metadata .in").text(season);
+}
+
+function showNext() {
+    if (quoteID < quotes.length-1) { quoteID += 1; } else { quoteID = 0; }
+    showQuote(quoteID);
+}
+
+function showPrev() {
+    if (quoteID == 0 ) {quoteID = quotes.length-1; } else { quoteID -= 1; }
+    showQuote(quoteID);
 }
 
 function toggleFilter(e){
