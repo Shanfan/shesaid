dc0d1210b250225d9dbf0a18907f9e8801db7bf9
Shanfan Huang <chitchy@gmail.com>
2013-05-12 12:34:17 -0300

    resolve the conflicts

--- js/data/quotes.js
+++ js/data/quotes.js
@@ -257,11 +257,7 @@ var allQuotes = [
     "line": "How does it happen that four smart women have nothing to talk about but boyfriends? It’s like seventh grade but with bank accounts."
   },
   {
-<<<<<<< HEAD
-    "by": charas[0],
-=======
   	"by": charas[0],
->>>>>>> Updated about page
   	"inSeason": 6,
   	"line": "Over the years I have bought Kyra an engagement gift, a wedding gift, then there was the trip to Maine for the wedding, three baby gifts. In total, I have spent over 2,300 dollars celebrating her choices. And she is shaming me for spending a lousy 485 bucks on myself?"
   },
